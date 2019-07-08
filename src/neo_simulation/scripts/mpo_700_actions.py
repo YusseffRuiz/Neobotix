9816e35b83d78d127c2740c3510f41795ca3283c
YusseffRuiz <zeiverking@gmail.com>
2019-07-08 22:43:41 +0100

    Define different Actions Based on the coordinates.
    Change variables:
    moveDistance = to modify meters to move
    moveAngle = 10 degrees aprox
    
    They move and then stop after moving the desired distance.
    
    These actions will be for the NN training.

--- src/neo_simulation/scripts/mpo_700_actions.py
+++ src/neo_simulation/scripts/mpo_700_actions.py
@@ -2,7 +2,6 @@
 
 import rospy
 import math
-from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import Point, Twist
@@ -13,19 +12,37 @@ import time
 
 class RobotActions:
 
+
+
     def __init__(self):
 
         # Creates a node with name 'speed_controller' and make sure it is a
         # unique node (using anonymous=True).
         rospy.init_node("robotActions", anonymous=True)
         # Publisher which will publish to the topic '/cmd_vel'.
-
+        self.pose_subscriber = rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.getPosition)
         self.velocity_publisher = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
         # A subscriber to the topic '/amcl_pose'. self.update_pose is called
         # when a message of type Pose is received.
         self.r = rospy.Rate(10)
         self.speed = Twist()
+        self.myPose = Point()
+        self.moveDistance = 1.0
+        self.moveAngle = (2 * math.pi) / 36
 
+
+
+    def getPosition(self, msg):
+        # Add these lines if required to use x and y
+        x = msg.pose.pose.position.x
+        y = msg.pose.pose.position.y
+
+
+        rot_q = msg.pose.pose.orientation
+        (roll, pitch, theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
+        self.myPose.x = x
+        self.myPose.y = y
+        self.myPose.z = theta
 
     def publish_vel(self, x, y, z):
         self.speed.linear.x = x
@@ -44,22 +61,59 @@ class RobotActions:
         rospy.spin
 
     def moveFront(self):
+        end_flag = False
+        end_distance = self.myPose.x + self.moveDistance
         self.publish_vel(0.8, 0.0, 0.0)
+        while not end_flag:
+            if self.myPose.x >= end_distance:
+                end_flag = True
+        self.stopMove()
 
     def moveLeft(self):
+        end_flag = False
+        end_distance = self.myPose.y + self.moveDistance/2
         self.publish_vel(0.0, 0.8, 0.0)
+        while not end_flag:
+            if self.myPose.y >= end_distance:
+                end_flag = True
+        self.stopMove()
 
     def moveRight(self):
+        end_flag = False
+        end_distance = self.myPose.y - self.moveDistance/2
         self.publish_vel(0.0, -0.8, 0.0)
+        while not end_flag:
+            if self.myPose.y <= end_distance:
+                end_flag = True
+        self.stopMove()
+
 
     def moveBack(self):
+        end_flag = False
+        end_distance = self.myPose.x - self.moveDistance/2
         self.publish_vel(-0.8, 0.0, 0.0)
+        while not end_flag:
+            if self.myPose.x <= end_distance:
+                end_flag = True
+        self.stopMove()
 
     def spinRight(self):
+        end_flag = False
+        end_distance = self.myPose.z - self.moveAngle
         self.publish_vel(0.0, 0.0, -0.7)
+        while not end_flag:
+            if self.myPose.z <= end_distance:
+                end_flag = True
+        self.stopMove()
 
     def spinLeft(self):
+        end_flag = False
+        end_distance = self.myPose.z + self.moveAngle
         self.publish_vel(0.0, 0.0, 0.7)
+        while not end_flag:
+            if self.myPose.z >= end_distance:
+                end_flag = True
+        self.stopMove()
 
     def stopMove(self):
         self.publish_vel(0.0, 0.0, 0.0)
@@ -89,7 +143,6 @@ if __name__ == "__main__":
             print("move: ", y)
             time.sleep(2)
 
-        x.spinRight()
         time.sleep(4)
 
         x.resetWorld()
