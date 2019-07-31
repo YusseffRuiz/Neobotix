e202e3c14b03d99c5cb7c344ed0dfee6487ac395
YusseffRuiz <zeiverking@gmail.com>
2019-07-31 17:50:00 +0100

    Adding Map and new ARS File

---
+++ build/neobotix_simulations/neobotix_fake/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yusseff/Neobotix/src/neobotix_simulations/neobotix_fake
+# Build directory: /home/yusseff/Neobotix/build/neobotix_simulations/neobotix_fake
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/neobotix_simulations/neobotix_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yusseff/Neobotix/src/neobotix_simulations/neobotix_gazebo
+# Build directory: /home/yusseff/Neobotix/build/neobotix_simulations/neobotix_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
